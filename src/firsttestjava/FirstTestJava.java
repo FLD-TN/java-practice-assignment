fa84acee0be2e33f3705d6d28f7cc858dfb2a5da
FLD-TN <duyhsne@gmail.com>
2025-04-26 18:14:13 +0700

    first commit

---
+++ src/firsttestjava/FirstTestJava.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firsttestjava;
+
+/**
+ *
+ * @author Admin
+ */
+public class FirstTestJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        int a =0;
+        int b =1;
+        System.out.println("Ket qua la : "+ (a+b));
+      
+        }
+    }
+    
+

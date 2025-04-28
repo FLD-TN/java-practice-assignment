3a5f42221b2919d94905a4b1d2eb96a3411bca35
FLD-TN <duyhsne@gmail.com>
2025-04-28 07:26:48 +0700

    update file

--- src/firsttestjava/Day1_PhepGan_PhepSoSanh.java
+++ src/firsttestjava/Day1_PhepGan_PhepSoSanh.java
@@ -4,10 +4,26 @@
  */
 package firsttestjava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
  */
 public class Day1_PhepGan_PhepSoSanh {
-    
+
+    public static void main(String args[]) {
+        int x = 4;
+        x += 5;
+        System.out.println("Kết quả sau khi dùng x+=5 là :" + x);
+        x -= 5;
+        System.out.println("Kết quả sau khi dùng x-=5 là (x trước khi trừ là 9) :" + x);
+        x *= 2;
+        System.out.println("Kết quả sau khi dùng x*=2 là ( x trước khi nhân là 4) :" + x);
+        x /= 2;
+        System.out.println("Kết quả sau khi x/=2 là ( x trước khi chia lấy nguyên là 8) :" + x);
+        x%=3;
+        System.out.println("Kết quả sau khi x%=3 là ( x trước khi chia lấy dư là 4) :"+ x);
+    }
+
 }

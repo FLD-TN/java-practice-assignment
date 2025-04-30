5ee50b7fa6baf189d060d0062d0a9b267580b85e
FLD-TN <duyhsne@gmail.com>
2025-04-30 21:31:21 +0700

    doc: update về bài tập về mảng 1 chiều

--- src/firsttestjava/Day3_Array.java
+++ src/firsttestjava/Day3_Array.java
@@ -44,33 +44,68 @@ class mang1Chieu {
         System.out.println("Nhập số phần tử muốn khởi tạo mảng: ");
         int n = input.nextInt();
         int[] arr = new int[n];
-        for(int i=0;i<arr.length;i++)
-        {
-            System.out.println("Nhập phần tử thứ"+i+": ");
-            arr[i]=input.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Nhập phần tử thứ" + i + ": ");
+            arr[i] = input.nextInt();
         }
-        
-        System.out.print("Mảng ban đầu đã tạo: ");
-        for(int i =0;i<arr.length;i++)
-        {
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println(" ");
-        
-        System.out.print("Mảng sau khi đảo ngược: ");
-        int temp=0;
-        for(int i =0;i<arr.length/2;i++)
-        {
-            temp = arr[i];
-             arr[i]= arr[arr.length-1-i] ;
-            arr[arr.length-1-i] = temp;
-        }
-        for(int i =0;i<arr.length;i++)
-        {
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println(" ");
-        
 
+        // khởi tạo mảng
+        System.out.print("Mảng ban đầu đã tạo: ");
+        System.out.println(Arrays.toString(arr));
+
+        //đảo ngược mảng
+        System.out.print("Mảng sau khi đảo ngược: ");
+        int temp = 0;
+        for (int i = 0; i < arr.length / 2; i++) {
+            temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+
+        //sắp xếp mảng tăng dần
+        System.out.print("Mảng sau khi sắp xếp tăng dần: ");
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+        //Tính tổng các phần tử trong mảng
+        System.out.print("Tổng các phần tử trong mảng là: ");
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.print(sum);
+        System.out.println(" ");
+    
+        
+        //Cho người dùng nhập 1 số bất kì 
+        // kiểm tra số đó có trong mảng hay không
+        // nếu có thì xuất ra vị trí index số đó trong mảng
+        
+        System.out.println("Nhập 1 số muốn kiểm tra: ");
+        int x = input.nextInt();
+        boolean flag = false;
+        for(int i =0;i<arr.length;i++)
+        {   
+            if(arr[i]==x)
+            {
+               System.out.println("Số "+x+" có tồn tại trong mảng\nvà nằm ở index: "+i);
+                flag=true;
+                break;
+            }
+        }
+        if(flag==false){
+            System.out.println("Số "+x+" không có trong mảng");   
+        }
     }
 }
+   
+
